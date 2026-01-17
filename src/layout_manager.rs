9629ad73253da42c9983108edfc0058b522780fd
Losmi <milosnikolic@milosnikolic.de>
2026-01-17 22:53:40 +0100

    refactor: moved the splitting of the frame here from app.rs

---
+++ src/layout_manager.rs
@@ -0,0 +1,38 @@
+use ratatui::prelude::*;
+
+// Splits the home area into 3 chunks and returns them, allowing for draw functions to be called on those chunks
+// Layout:
+/*
+
+Frame:
+-----------------------
+| horizontal_split[0] | 50%
+-----------------------
+| horizontal_split[1] | 50%
+-----------------------
+
+vertical_split[1]:
+-------------------------------------------------------
+| bottom_vertical_split[0] | bottom_vertical_split[1] |
+-------------------------------------------------------
+            50%                        50%
+*/
+pub fn split_home_area(area: Rect) -> [Rect; 3] {
+    // 1. Splits the home view into horizontal halves
+    let horizontal_split = Layout::default()
+        .direction(Direction::Horizontal)
+        .constraints([Constraint::Percentage(50), Constraint::Percentage(50)])
+        .split(area);
+
+    // 2. Splits the bottom half into horizontal halves
+    let bottom_vertical_split = Layout::default()
+        .direction(Direction::Horizontal)
+        .constraints([Constraint::Percentage(50), Constraint::Percentage(50)])
+        .split(horizontal_split[0]);
+
+    [
+        horizontal_split[0],
+        bottom_vertical_split[0],
+        bottom_vertical_split[1],
+    ]
+}
